557ab19d1383e7f1ef110be5c1266d17068c244e
roelandverbakel <roelandverbakel@gmail.com>
2017-02-03 19:54:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        var target = "_blank";
-        var options = "location=yes, closebuttoncaption=";
+        var target = "_self";
+        var options = "location=yes";
         var ref = cordova.InAppBrowser.open('http://www.stichtingtoegankelijkehoreca.nl', target, options);
         ref.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
         ref.addEventListener('exit', function(event) { alert(event.type); });
