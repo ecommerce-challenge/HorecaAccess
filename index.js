3ab8ea3540e24834d8f9d80ab8c6834cf8fd08a6
roelandverbakel <roelandverbakel@gmail.com>
2017-02-03 10:07:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        iab.open('http://www.stichtingtoegankelijkehoreca.nl', '_blank');
         cordova.InAppBrowser.open('http://www.stichtingtoegankelijkehoreca.nl', '_blank', 'location=yes')
     }
     
