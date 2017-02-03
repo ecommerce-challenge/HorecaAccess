0ea4930508e3a17924ffc904c5f7d6a823d8b6e7
roelandverbakel <roelandverbakel@gmail.com>
2017-02-03 09:47:59 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        cordova.InAppBrowser.open('http://www.stichtingtoegankelijkehoreca.nl', '_blank', 'location=yes')
+    }
+    
+};
